e48504e3fdb09394e7c64208d51102c80a87d364
YevheniiKhablak <102540628+YevheniiKhablak@users.noreply.github.com>
2022-06-16 14:55:37 +0300

    test

---
+++ file.js
@@ -0,0 +1,58 @@
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    ganres: [],
+    privat: false
+};
+
+const a = prompt('Один из постедних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов', ''),
+      d = prompt('На сколько оцените его?', '');
+
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;
+
+      console.log(personalMovieDB);
+
+      const num1 = 140;
+      if (num <49) {
+        console.log('Error');
+      }
+      else if (num > 100) {
+        console.log('Many');
+      }
+      else {
+        console.log('Ok');
+      }
+
+      (num === 50) ? console.log('Ok') : console.log('Error');
+
+
+      
+      const num = 50;  
+
+      switch (num) {      
+        case 49:
+            console.log('no');
+            break;
+        case 500:
+            console.log('no');
+            break;
+        case 100:
+            console.log('no');
+            break;
+        default:
+            console.log('not a your day bro');
+            break;
+      }
+
+      let johnReport, alexReport, samReport = 'done';
+
+      console.log(johnReport || alexReport || samReport);
+
+      alert (hello);
+
